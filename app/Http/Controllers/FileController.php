f6d6788bd2349da2f9316a76b1f8063e77eaa77b
ludoguenet <ludovicguenet@gmx.com>
2024-11-02 13:56:37 +0100

    initial start 🚀

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -18,29 +18,11 @@ final class FileController extends Controller
 
     public function store()
     {
-        $validated = request()->validate([
-            'file' => [
-                'required',
-                'max:5120',
-                'mimes:pdf',
-            ],
-        ]);
-
-        $file = $validated['file'];
-        $name = $file->getClientOriginalName();
-
-        File::create([
-            'name' => $name,
-            'path' => $file->storeAs('uploads', $name),
-        ]);
-
-        return back();
+        //
     }
 
     public function download(File $file)
     {
-        dd(Storage::temporaryUrl($file->path, now()->addMinute()));
-
-        return redirect(Storage::temporaryUrl($file->path, now()->addHour()));
+        //
     }
 }
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('files.index')" :active="request()->routeIs('files.index')">
+                        {{ __('Uploads') }}
+                    </x-nav-link>
                 </div>
             </div>
 
