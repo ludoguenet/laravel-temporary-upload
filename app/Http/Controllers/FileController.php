75295b302a8f0fb24fc740d19037c8f7e0078c44
ludoguenet <ludovicguenet@gmx.com>
2024-11-02 14:41:17 +0100

    fix

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -18,11 +18,27 @@ final class FileController extends Controller
 
     public function store()
     {
-        //
+        $validated = request()->validate([
+            'file' => [
+                'required',
+                'max:5120', // 5 MB
+                'mimes:pdf',
+            ],
+        ]);
+
+        $file = $validated['file'];
+        $name = $file->getClientOriginalName();
+
+        File::create([
+            'name' => $name,
+            'path' => $file->storeAs('uploads', $name),
+        ]);
+
+        return back();
     }
 
     public function download(File $file)
     {
-        //
-    }
+        return redirect(Storage::temporaryUrl($file->path, now()->addHour()));
+     }
 }
